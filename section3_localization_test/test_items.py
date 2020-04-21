28b2346790f69f187db15d0f30ea604b1dc798c2
hemera <hemerocallisongml@gmail.com>
2020-04-21 08:20:18 +0700

    test code review

--- section3_localization_test/test_items.py
+++ section3_localization_test/test_items.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-def test_localization(browser):
+def test_guest_can_see_btn_add_to_busket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(3)
 
-    button_add = browser.find_element(By.XPATH, "//button[@type='submit' and @class='btn btn-lg btn-primary btn-add-to-basket']")
+    button_add = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert button_add != None, "There is no such element on the page."
